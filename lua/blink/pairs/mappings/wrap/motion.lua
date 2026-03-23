25c4b8b11e7a0b0f19976ceb1f57f0f785685dd7
Liam Dyer <liamcdyer@gmail.com>
2026-03-23 15:04:51 -0400

    feat(wrap): drop `move_cursor`, get pair in operator
    Dropping the option for `move_cursor` to simplify the config validation.
    I'd like to add this back later, but as an argument to a function, which
    can be used via `vim.keymap.set` rather than through the declarative config.

--- lua/blink/pairs/mappings/wrap/motion.lua
+++ lua/blink/pairs/mappings/wrap/motion.lua
@@ -4,28 +4,26 @@ local motions = {}
 
 --- @type [integer, integer]
 local cursor
---- @type blink.pairs.WrapOpts?
-local wrap_opts
+--- @type blink.pairs.WrapType?
+local wrap_type
 --- @type 'forward' | 'backward' | nil
 local direction
 
+--- @param pos [integer, integer]
 --- @param col_offset? integer
 --- @return blink.pairs.MatchWithLine[]?
-function motions.get_pair_at_cursor(col_offset)
-  local cursor = vim.api.nvim_win_get_cursor(0)
+function motions.get_pair_at(pos, col_offset)
   local bufnr = vim.api.nvim_get_current_buf()
-  if col_offset then cursor[2] = math.max(cursor[2] + col_offset, 0) end
-  return rust.get_surrounding_match_pair(bufnr, cursor[1] - 1, cursor[2])
+  return rust.get_surrounding_match_pair(bufnr, pos[1] - 1, math.max(pos[2] + (col_offset or 0), 0))
 end
 
 --- Perform setup for the wrap operator, getting the cursor position, storing options
 --- and clearing state for dot-repeat
---- @param opts blink.pairs.WrapOpts
---- @param cursor_? [integer, integer]
-function motions.set_operator_wrap(opts, cursor_)
-  cursor = cursor_ or vim.api.nvim_win_get_cursor(0)
+--- @param type blink.pairs.WrapType
+function motions.set_operator_wrap(type)
+  cursor = vim.api.nvim_win_get_cursor(0)
   cursor[2] = math.max(0, cursor[2] - 1)
-  wrap_opts = opts
+  wrap_type = type
   direction = nil
 
   vim.o.operatorfunc = 'v:lua.blink_pairs_wrap'
@@ -35,16 +33,15 @@ end
 -- Forward wrap operator: moves pair character at start_pos to motion end
 _G.blink_pairs_wrap = function()
   -- called without calling `motions.set_operator_wrap` first
-  if not wrap_opts or not cursor then return end
-
-  local pair = motions.get_pair_at_cursor(wrap_opts.type == 'motion_reverse' and -1 or nil)
-  if not pair or #pair ~= 2 then return end
-  pair = wrap_opts.type == 'motion_reverse' and pair[1] or pair[2]
-  local pair_pos = { pair.line, pair.col }
+  if not wrap_type or not cursor then return end
 
   local motion_start_pos = vim.api.nvim_buf_get_mark(0, '[') -- start of operated region
   local motion_end_pos = vim.api.nvim_buf_get_mark(0, ']') -- end of operated region
   if motion_start_pos[1] == 0 or motion_end_pos[1] == 0 then return end -- not set, didn't complete motion
+
+  -- if we're running for the first time, we must be in insert mode, and not in normal mode doing dot-repeat
+  -- if we're at `(|'')`, we want to select the `(`, not the `'`, so we offset backwards by 1
+  local pair_col_offset = direction == nil and wrap_type == 'motion_reverse' and -1 or 0
 
   -- when running the operator for the first time, the global `cursor` variable will let us figure out if
   -- the direction is forward or backward
@@ -55,6 +52,14 @@ _G.blink_pairs_wrap = function()
   local new_pair_pos = direction == 'backward' and motion_start_pos or { motion_end_pos[1], motion_end_pos[2] + 1 }
   new_pair_pos[1] = new_pair_pos[1] - 1 -- convert to 0-indexed
 
+  local pair = motions.get_pair_at(
+    direction == 'backward' and { motion_end_pos[1], motion_end_pos[2] + 1 } or motion_start_pos,
+    pair_col_offset
+  )
+  if not pair or #pair ~= 2 then return end
+  pair = wrap_type == 'motion_reverse' and pair[1] or pair[2]
+  local pair_pos = { pair.line, pair.col }
+
   -- clamp to end of line
   local line_len = #vim.api.nvim_buf_get_lines(0, new_pair_pos[1], new_pair_pos[1] + 1, true)[1]
   new_pair_pos[2] = math.min(line_len, new_pair_pos[2])
@@ -63,7 +68,10 @@ _G.blink_pairs_wrap = function()
   local paren = vim.api.nvim_buf_get_text(0, pair_pos[1], pair_pos[2], pair_pos[1], pair_pos[2] + 1, {})[1]
   vim.api.nvim_buf_set_text(0, new_pair_pos[1], new_pair_pos[2], new_pair_pos[1], new_pair_pos[2], { paren })
 
-  -- clear parenthesis at the original position
+  -- move cursor to the pair
+  vim.api.nvim_win_set_cursor(0, { new_pair_pos[1] + 1, new_pair_pos[2] + (wrap_type == 'motion_reverse' and 1 or 0) })
+
+  -- clear pair at the original position
   if pair_pos[1] == new_pair_pos[1] and pair_pos[2] > new_pair_pos[2] then
     -- compensate for the new position being 1 character to the right of the original position
     -- since we inserted the character at the new position
@@ -71,12 +79,6 @@ _G.blink_pairs_wrap = function()
     new_pair_pos[2] = new_pair_pos[2] + 1
   end
   vim.api.nvim_buf_set_text(0, pair_pos[1], pair_pos[2], pair_pos[1], pair_pos[2] + 1, {})
-
-  if wrap_opts and wrap_opts.move_cursor == false then
-    vim.api.nvim_win_set_cursor(0, { pair_pos[1] + 1, math.max(0, pair_pos[2] - 2) })
-  else
-    vim.api.nvim_win_set_cursor(0, { new_pair_pos[1] + 1, math.max(0, new_pair_pos[2] - 1) })
-  end
 end
 
 return motions
