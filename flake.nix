1e8ad765ab956e87b0c7b86a32ffc5c265d9519b
Stefan Boca <45266795+stefanboca@users.noreply.github.com>
2026-05-01 14:35:26 -0700

    feat(nix): refactor (#98)
    remove flake parts, add an overlay and checks. users of the overlay need
    to apply `fenix.overlays.default` as well. If they don't declare a fenix
    input directly, they can use `blink-pairs.inputs.fenix.overlays.default`.

--- flake.nix
+++ flake.nix
@@ -2,108 +2,106 @@
   description = "Rainbow highlighting and intelligent auto-pairs for Neovim";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+
     fenix.url = "github:nix-community/fenix";
     fenix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ flake-parts, nixpkgs, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems =
-        [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+  outputs = {
+    nixpkgs,
+    fenix,
+    self,
+    ...
+  }: let
+    inherit (nixpkgs) lib;
+    inherit (lib.attrsets) genAttrs mapAttrs' nameValuePair;
+    inherit (lib.fileset) fileFilter toSource unions;
+    inherit (lib.strings) hasPrefix;
 
-      perSystem = { self, config, self', inputs', pkgs, system, lib, ... }: {
-        # use fenix overlay
-        _module.args.pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ inputs.fenix.overlays.default ];
-        };
+    systems = ["x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin"];
+    forAllSystems = genAttrs systems;
+    nixpkgsFor = forAllSystems (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [fenix.overlays.default];
+      });
 
-        # define the packages provided by this flake
-        packages = let
-          fs = lib.fileset;
-          # nix source files (*.nix)
-          nixFs = fs.fileFilter (file: file.hasExt == "nix") ./.;
-          # rust source files
-          rustFs = fs.unions [
-            # Cargo.*
-            (fs.fileFilter (file: lib.hasPrefix "Cargo" file.name) ./.)
-            # *.rs
-            (fs.fileFilter (file: file.hasExt "rs") ./.)
-            # additional files
-            ./.cargo
-          ];
-          # nvim source files
-          # all that are not nix, nor rust, nor other ignored files
-          nvimFs = fs.difference ./. (fs.unions [ nixFs rustFs ./repro.lua ]);
-          version = "0.4.1";
-        in {
-          blink-pairs-lib = let
-            inherit (inputs.fenix.packages.${system}.minimal) toolchain;
-            rustPlatform = pkgs.makeRustPlatform {
-              cargo = toolchain;
-              rustc = toolchain;
-            };
-          in rustPlatform.buildRustPackage {
-            pname = "blink-pairs-lib";
-            inherit version;
-            src = fs.toSource {
-              root = ./.;
-              fileset = rustFs;
-            };
-            cargoLock = { lockFile = ./Cargo.lock; };
-            doCheck = false;
-          };
-
-          blink-pairs = pkgs.vimUtils.buildVimPlugin {
-            pname = "blink.pairs";
-            inherit version;
-            src = fs.toSource {
-              root = ./.;
-              fileset = nvimFs;
-            };
-            preInstall = ''
-              mkdir -p target/release
-              ln -s ${self'.packages.blink-pairs-lib}/lib/libblink_pairs.* target/release/
-            '';
-          };
-
-          default = self'.packages.blink-pairs;
-        };
-
-        # builds the native module of the plugin
-        apps.build-plugin = {
-          type = "app";
-          program = let
-            buildScript = pkgs.writeShellApplication {
-              name = "build-plugin";
-              runtimeInputs = with pkgs; [ fenix.minimal.toolchain gcc ];
-              text = ''
-                export LIBRARY_PATH="${lib.makeLibraryPath [ pkgs.libiconv ]}";
-                cargo build --release
-              '';
-            };
-          in (lib.getExe buildScript);
-        };
-
-        # define the default dev environment
-        devShells.default = pkgs.mkShell {
-          name = "blink";
-          inputsFrom = [
-            self'.packages.blink-pairs-lib
-            self'.packages.blink-pairs
-            self'.apps.build-plugin
-          ];
-          packages = with pkgs; [ rust-analyzer ];
-        };
-
-        formatter = pkgs.nixfmt-classic;
+    version = "0.4.1";
+    blink-pairs-package = {
+      fenix,
+      makeRustPlatform,
+      vimUtils,
+    }: let
+      inherit (fenix.minimal) toolchain;
+      rustPlatform = makeRustPlatform {
+        cargo = toolchain;
+        rustc = toolchain;
       };
+    in
+      vimUtils.buildVimPlugin {
+        pname = "blink.pairs";
+        inherit version;
+        src = toSource {
+          root = ./.;
+          fileset = fileFilter (file: file.hasExt "lua") ./lua;
+        };
+
+        preInstall = ''
+          mkdir -p target/release
+          ln -s $rust_lib/lib/libblink_pairs.* target/release/
+        '';
+
+        env.rust_lib = rustPlatform.buildRustPackage {
+          pname = "blink-pairs-lib";
+          inherit version;
+          src = toSource {
+            root = ./.;
+            fileset = unions [
+              (fileFilter (file: file.hasExt "rs") ./.)
+              (fileFilter (file: hasPrefix "Cargo" file.name) ./.) # Cargo.*
+              ./.cargo
+            ];
+          };
+          cargoLock.lockFile = ./Cargo.lock;
+          doCheck = false;
+        };
+
+        passthru = {inherit rustPlatform;};
+      };
+  in {
+    packages = forAllSystems (system: rec {
+      blink-pairs = nixpkgsFor.${system}.callPackage blink-pairs-package {};
+      default = blink-pairs;
+    });
+
+    overlays.default = final: prev: {
+      vimPlugins = prev.vimPlugins.extend (_: _: {
+        blink-pairs = final.callPackage blink-pairs-package {};
+      });
     };
 
+    devShells = forAllSystems (
+      system: let
+        pkgs = nixpkgsFor.${system};
+        packages = self.packages.${system};
+      in {
+        default = pkgs.mkShell {
+          name = "blink";
+          inputsFrom = [
+            packages.blink-pairs
+            packages.blink-pairs.rust_lib
+          ];
+          packages = [pkgs.fenix.rust-analyzer];
+        };
+      }
+    );
+
+    checks = forAllSystems (system: mapAttrs' (n: nameValuePair "package-${n}") (removeAttrs self.packages.${system} ["default"]));
+  };
+
   nixConfig = {
-    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-substituters = ["https://nix-community.cachix.org"];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs"
     ];
