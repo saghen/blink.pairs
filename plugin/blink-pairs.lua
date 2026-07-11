a458bfb15b5e99df1e0755839b3fd81866d063bc
Melker Widén <melker.widen@gmail.com>
2026-07-11 16:50:39 +0200

    fix: use global namespace for highlight definitions

--- plugin/blink-pairs.lua
+++ plugin/blink-pairs.lua
@@ -1,10 +1,9 @@
 local function set_highlights()
-  local ns = vim.api.nvim_create_namespace('blink_pairs')
-  vim.api.nvim_set_hl(ns, 'BlinkPairsOrange', { ctermfg = 15, fg = '#d65d0e', default = true })
-  vim.api.nvim_set_hl(ns, 'BlinkPairsPurple', { ctermfg = 13, fg = '#b16286', default = true })
-  vim.api.nvim_set_hl(ns, 'BlinkPairsBlue', { ctermfg = 12, fg = '#458588', default = true })
-  vim.api.nvim_set_hl(ns, 'BlinkPairsUnmatched', { ctermfg = 9, fg = '#ff007c', default = true })
-  vim.api.nvim_set_hl(ns, 'BlinkPairsMatchParen', { link = 'MatchParen', default = true })
+  vim.api.nvim_set_hl(0, 'BlinkPairsOrange', { ctermfg = 15, fg = '#d65d0e', default = true })
+  vim.api.nvim_set_hl(0, 'BlinkPairsPurple', { ctermfg = 13, fg = '#b16286', default = true })
+  vim.api.nvim_set_hl(0, 'BlinkPairsBlue', { ctermfg = 12, fg = '#458588', default = true })
+  vim.api.nvim_set_hl(0, 'BlinkPairsUnmatched', { ctermfg = 9, fg = '#ff007c', default = true })
+  vim.api.nvim_set_hl(0, 'BlinkPairsMatchParen', { link = 'MatchParen', default = true })
 end
 
 set_highlights()
